d8b839bebe93473dba1cb852d9e61ace929fc8c8
Yusuf Eliaçık <yusuf@fireflyon.com>
2022-03-20 01:42:42 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useTranslation } from "react-i18next";
 
-function App() {
+const App = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => changeLanguage("tr")}>Select TR Language</button>
+      <button onClick={() => changeLanguage("en")}>Select EN Language</button>
+      <button onClick={() => changeLanguage("de")}>Select DE Language</button>
+
+      <div className="example-text">{t("example.paragraph")}</div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/locales/index.ts
@@ -0,0 +1,5 @@
+import EN from "./en.json";
+import TR from "./tr.json";
+import DE from "./de.json";
+
+export { EN, TR, DE };
